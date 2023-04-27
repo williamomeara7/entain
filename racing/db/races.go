4a38f27ebdcd4cfd65f8de0677c3ea9f8c672d6d
williamomeara7 <william.omeara@ucdconnect.ie>
2023-04-27 11:09:48 +1000

    We'd like to see the races returned, ordered by their `advertised_start_time`    > Bonus points if you allow the consumer to specify an ORDER/SORT-BY they might be after.

--- racing/db/races.go
+++ racing/db/races.go
@@ -17,7 +17,7 @@ type RacesRepo interface {
 	Init() error
 
 	// List will return a list of races.
-	List(filter *racing.ListRacesRequestFilter) ([]*racing.Race, error)
+	List(filter *racing.ListRacesRequestFilter, sort *racing.ListRacesRequestSort) ([]*racing.Race, error)
 }
 
 type racesRepo struct {
@@ -42,7 +42,7 @@ func (r *racesRepo) Init() error {
 	return err
 }
 
-func (r *racesRepo) List(filter *racing.ListRacesRequestFilter) ([]*racing.Race, error) {
+func (r *racesRepo) List(filter *racing.ListRacesRequestFilter, sort *racing.ListRacesRequestSort) ([]*racing.Race, error) {
 	var (
 		err   error
 		query string
@@ -52,6 +52,8 @@ func (r *racesRepo) List(filter *racing.ListRacesRequestFilter) ([]*racing.Race,
 	query = getRaceQueries()[racesList]
 
 	query, args = r.applyFilter(query, filter)
+
+	query = r.applySorting(query, sort)
 
 	rows, err := r.db.Query(query, args...)
 	if err != nil {
@@ -89,6 +91,41 @@ func (r *racesRepo) applyFilter(query string, filter *racing.ListRacesRequestFil
 	}
 
 	return query, args
+}
+
+func (r *racesRepo) applySorting(query string, sort *racing.ListRacesRequestSort) string {
+	if sort == nil {
+		return query
+	}
+
+	if sort.Field == nil {
+		return query
+	}
+
+	switch *sort.Field.Enum() {
+	case racing.ListRacesRequestSort_ADVERTISED_START_TIME:
+		query += " ORDER BY advertised_start_time"
+	case racing.ListRacesRequestSort_ID:
+		query += " ORDER BY id"
+	case racing.ListRacesRequestSort_NAME:
+		query += " ORDER BY name"
+	case racing.ListRacesRequestSort_NUMBER:
+		query += " ORDER BY number"
+	}
+
+	if sort.Order == nil {
+		return query
+	}
+	switch *sort.Order.Enum() {
+	case racing.ListRacesRequestSort_ASC:
+		query += " ASC"
+	case racing.ListRacesRequestSort_DESC:
+		query += " DESC"
+	}
+
+	return query
+
+	return query
 }
 
 func (m *racesRepo) scanRaces(
